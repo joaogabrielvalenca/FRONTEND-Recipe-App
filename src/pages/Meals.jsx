26dfeae51e8fad463829feefce240764f85cd71b
Gabriella Vieira <gabvi2015@outlook.com>
2023-05-05 18:58:29 -0300

    fix: conflicts

--- src/pages/Meals.jsx
+++ src/pages/Meals.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import Header from '../components/Header';
-=======
 import Footer from '../components/Footer';
->>>>>>> main-group-1
 
 function Meals() {
   return (
     <div>
-<<<<<<< HEAD
       <Header />
-=======
-      Meals
       <Footer />
->>>>>>> main-group-1
     </div>
   );
 }
