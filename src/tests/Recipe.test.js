71bd103ef43af64c7e51259437d7ab4d19ccadea
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-06 18:30:32 -0300

    test: req 20

--- src/tests/Recipe.test.js
+++ src/tests/Recipe.test.js
@@ -3,22 +3,31 @@ import { screen } from '@testing-library/react';
 import RecipeProvider from '../context/RecipeProvider';
 import renderWithRouter from './helpers/renderWithRouter';
 import meals from './mocks/meals';
-// import drinks from './mocks/drinks';
+import drinks from './mocks/drinks';
 import mealsCategories from './mocks/mealsCategories';
-// import drinksCategories from './mocks/drinksCategories';
-// import Meals from '../pages/Meals';
+import drinksCategories from './mocks/drinksCategories';
 import App from '../App';
+// import errorMessage from './mocks/errorMessage';
 
-describe('the meals component', () => {
+describe('the recipe component', () => {
   beforeEach(() => {
     jest.spyOn(global, 'fetch').mockResolvedValueOnce({
       json: async () => (meals),
     }).mockResolvedValueOnce({
       json: async () => (mealsCategories),
-    });
+    }).mockResolvedValueOnce({
+      json: async () => (drinks),
+    })
+      .mockResolvedValueOnce({
+        json: async () => (drinksCategories),
+      });
   });
 
-  it('should fetch twice on page load', async () => {
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it('should fetch twice on meals page load', async () => {
     renderWithRouter(<RecipeProvider><App /></RecipeProvider>, { initialEntries: ['/meals'] });
     expect(fetch).toHaveBeenCalledTimes(2);
 
@@ -30,5 +39,18 @@ describe('the meals component', () => {
 
     const corbaMeal = await screen.findByRole('heading', { name: /corba/i });
     expect(corbaMeal).toBeInTheDocument();
+  });
+  it('should fetch twice on drinks page load', async () => {
+    renderWithRouter(<RecipeProvider><App /></RecipeProvider>, { initialEntries: ['/drinks'] });
+    expect(fetch).toHaveBeenCalledTimes(2);
+
+    const loadingEl = screen.getByRole('status');
+    expect(loadingEl).toBeInTheDocument();
+
+    const ordinaryDrinkBtn = await screen.findByRole('button', { name: /ordinary/i });
+    expect(ordinaryDrinkBtn).toBeInTheDocument();
+
+    const ggDrink = await screen.findByRole('heading', { name: /GG/i });
+    expect(ggDrink).toBeInTheDocument();
   });
 });
