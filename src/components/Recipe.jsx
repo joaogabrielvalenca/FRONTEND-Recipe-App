6b07d66e14c2189e0747f078bae1ce98180dc0c9
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-06 10:30:49 -0300

    feat: req 20, create buttons

--- src/components/Recipe.jsx
+++ src/components/Recipe.jsx
@@ -4,36 +4,70 @@ import { RecipeContext } from '../context/RecipeProvider';
 
 function Recipe() {
   const { location: { pathname } } = useHistory();
-  const { mealsData, drinksData } = useContext(RecipeContext);
+  const {
+    mealsData, drinksData, mealsCategories, drinksCategories,
+  } = useContext(RecipeContext);
   return (
-    <ul>
-      {pathname === '/meals'
-        ? mealsData.map((e, index) => (
-          <li
-            data-testid={ `${index}-recipe-card` }
-            key={ e.idMeal }
-          >
-            <h3 data-testid={ `${index}-card-name` }>{e.strMeal}</h3>
-            <img
-              src={ e.strMealThumb }
-              alt={ e.strMeal }
-              data-testid={ `${index}-card-img` }
-            />
-          </li>
-        )) : drinksData.map((e, index) => (
-          <li
-            data-testid={ `${index}-recipe-card` }
-            key={ e.idDrink }
-          >
-            <h3 data-testid={ `${index}-card-name` }>{e.strDrink}</h3>
-            <img
-              src={ e.strDrinkThumb }
-              alt={ e.strDrink }
-              data-testid={ `${index}-card-img` }
-            />
-          </li>
-        ))}
-    </ul>
+    <>
+      <nav>
+        {pathname === '/meals'
+          ? mealsCategories.map(({ strCategory }) => (
+            <button
+              data-testid={ `${strCategory}-category-filter` }
+              key={ strCategory }
+            >
+              {strCategory}
+            </button>
+          ))
+          : drinksCategories.map(({ strCategory }) => (
+            <button
+              data-testid={ `${strCategory}-category-filter` }
+              key={ strCategory }
+            >
+              {strCategory}
+            </button>
+          ))}
+      </nav>
+      <ul>
+        {pathname === '/meals'
+          ? mealsData.map((e, index) => (
+            <li
+              data-testid={ `${index}-recipe-card` }
+              key={ e.idMeal }
+
+            >
+              <h3 data-testid={ `${index}-card-name` }>{e.strMeal}</h3>
+              <figure className="figure">
+
+                <img
+                  width={ 200 }
+                  className="img-thumbnail figure-img img-fluid rounded"
+                  src={ e.strMealThumb }
+                  alt={ e.strMeal }
+                  data-testid={ `${index}-card-img` }
+                />
+              </figure>
+            </li>
+          )) : drinksData.map((e, index) => (
+            <li
+              data-testid={ `${index}-recipe-card` }
+              key={ e.idDrink }
+            >
+              <h3 data-testid={ `${index}-card-name` }>{e.strDrink}</h3>
+              <figure className="figure">
+
+                <img
+                  width={ 200 }
+                  className="img-thumbnail figure-img img-fluid rounded"
+                  src={ e.strDrinkThumb }
+                  alt={ e.strDrink }
+                  data-testid={ `${index}-card-img` }
+                />
+              </figure>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 }
 
--- src/context/RecipeProvider.js
+++ src/context/RecipeProvider.js
@@ -6,12 +6,17 @@ export const RecipeContext = createContext();
 
 const MEALS_URL = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const DRINKS_URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=';
+const MEALS_CATEGORIES = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+const DRINKS_CATEGORIES = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
 const MAX_ITEMS_QUANTITY = 12;
+const MAX_CATEGORIES_QUANTITY = 5;
 
 function RecipeProvider({ children }) {
   const { fetchApi } = useFetch();
   const [mealsData, setMealsData] = useState([]);
   const [drinksData, setDrinksData] = useState([]);
+  const [mealsCategories, setMealsCategories] = useState([]);
+  const [drinksCategories, setDrinksCategories] = useState([]);
 
   const getData = useCallback(async () => {
     const meals = await fetchApi(MEALS_URL);
@@ -20,13 +25,23 @@ function RecipeProvider({ children }) {
     setDrinksData(drinks.drinks.filter((_, index) => index < MAX_ITEMS_QUANTITY));
   }, [fetchApi]);
 
+  const getCategories = useCallback(async () => {
+    const mealsCategoriesData = await fetchApi(MEALS_CATEGORIES);
+    const drinksCategoriesData = await fetchApi(DRINKS_CATEGORIES);
+    setMealsCategories(mealsCategoriesData.meals
+      .filter((_, index) => index < MAX_CATEGORIES_QUANTITY));
+    setDrinksCategories(drinksCategoriesData.drinks
+      .filter((_, index) => index < MAX_CATEGORIES_QUANTITY));
+  }, [fetchApi]);
+
   useEffect(() => {
     getData();
+    getCategories();
   }, []);
 
   const values = useMemo(() => ({
-    mealsData, drinksData,
-  }), [mealsData, drinksData]);
+    mealsData, drinksData, mealsCategories, drinksCategories,
+  }), [mealsData, drinksData, mealsCategories, drinksCategories]);
 
   return (
     <RecipeContext.Provider value={ values }>
