4b627e40c98883a4b5939879affd8e3ab4d4534b
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-07 10:20:44 -0300

    fix: tests

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,15 @@ import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header() {
-  const location = window.location.pathname.slice(1);
+  const history = useHistory();
+  const location = history.location.pathname
+    .split('').filter((_e, index) => index !== 0).join('');
+  console.log(location);
   const title = location.includes('-')
     ? location.split('-').join(' ').replace(/(^\w{1})|(\s+\w{1})/g, (letra) => letra.toUpperCase())
     : location[0].toUpperCase() + location.substring(1);
 
   const withoutSearch = ['profile', 'done-recipes', 'favorite-recipes'];
-  const history = useHistory();
   return (
     <header>
       {/* lógica de renderização */}
@@ -18,12 +20,12 @@ function Header() {
         <img src={ profileIcon } alt="userImage" data-testid="profile-top-btn" />
       </button>
       <h1 data-testid="page-title">{title}</h1>
-      { !(withoutSearch.includes(location))
-      && (
-        <button type="button">
-          <img src={ searchIcon } alt="pesquisar" data-testid="search-top-btn" />
-        </button>
-      )}
+      {!(withoutSearch.includes(location))
+        && (
+          <button type="button">
+            <img src={ searchIcon } alt="pesquisar" data-testid="search-top-btn" />
+          </button>
+        )}
 
     </header>
   );
