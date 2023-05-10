e5a7fb681631345dd2eae70a57b0a4b35abf3a82
coelhoreinaldo <scoelhor7@gmail.com>
2023-05-10 12:12:13 -0300

    test: req 36

--- src/tests/RecipeDetails.test.js
+++ src/tests/RecipeDetails.test.js
@@ -5,10 +5,14 @@ import renderWithRouter from './helpers/renderWithRouter';
 import App from '../App';
 import Provider from '../context/Provider';
 import fetchMock from '../../cypress/mocks/fetch';
+import { favoriteRecipesStorage, recipesInProgressStorage } from './mocks/localStorageMocks';
 
-describe('the recipe details page', () => {
+const whiteHeartIcon = 'whiteHeartIcon.svg';
+
+describe('the meals recipe details page', () => {
   beforeEach(async () => {
     jest.spyOn(global, 'fetch').mockImplementation(fetchMock);
+    localStorage.clear();
   });
 
   it('should have the ingredients and measures', async () => {
@@ -34,8 +38,55 @@ describe('the recipe details page', () => {
     // expect(history.location.pathname).toBe('/meals/52977/in-progress');
     expect(history.location.pathname).toBe('/meals/52771/in-progress');
     // console.log(history);
+    // id diferente??
   });
 
+  it('should copy the meal recipe details page when the share button is clicked', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+    waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const corbaEl = await screen.findByRole('heading', { name: /corba/i });
+    userEvent.click(corbaEl);
+    waitForElementToBeRemoved(() => screen.getByRole('status'));
+
+    const shareBtn = await screen.findByTestId('share-btn');
+    expect(shareBtn).toBeInTheDocument();
+    const linkCopiedEl = screen.queryByText(/link copied/i);
+    expect(linkCopiedEl).not.toBeInTheDocument();
+  });
+
+  it('should verify if meal recipe it is in recipes in progress', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+    waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const corbaEl = await screen.findByRole('heading', { name: /corba/i });
+    userEvent.click(corbaEl);
+    waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const continueRecipeBtn = screen.queryByRole('button', { name: /continue/i });
+    expect(continueRecipeBtn).not.toBeInTheDocument();
+    localStorage.setItem('inProgressRecipes', JSON.stringify(recipesInProgressStorage));
+    const continueRecipeBtn2 = await screen.findByRole('button', { name: /continue/i });
+    expect(continueRecipeBtn2).toBeInTheDocument();
+    // estranhoooo
+  });
+
+  it('should verify if meal recipe is favorite', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+    await waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const corbaMeal = screen.getByRole('heading', { name: /corba/i });
+    userEvent.click(corbaMeal);
+    await waitForElementToBeRemoved(() => screen.getByRole('status'));
+
+    const favoriteBtn = screen.getByTestId('favorite-btn');
+    expect(favoriteBtn).toHaveAttribute('src', whiteHeartIcon);
+    userEvent.click(favoriteBtn);
+    expect(favoriteBtn).toHaveAttribute('src', 'blackHeartIcon.svg');
+  });
+});
+
+describe('the drinks recipe details page', () => {
+  beforeEach(async () => {
+    jest.spyOn(global, 'fetch').mockImplementation(fetchMock);
+    localStorage.clear();
+  });
   it('should redirect to drink recipe in progress page when the start recipe button is clicked', async () => {
     const { history } = renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/drinks'] });
     waitForElementToBeRemoved(() => screen.getByRole('status'));
@@ -49,20 +100,35 @@ describe('the recipe details page', () => {
     expect(startRecipeBtn).toBeInTheDocument();
     userEvent.click(startRecipeBtn);
     expect(history.location.pathname).toBe('/drinks/15997/in-progress');
-    // expect(history.location.pathname).toBe('/meals/52771/in-progress');
-    // console.log(history);
   });
 
-  it('should copy the recipe details page when the share button is clicked', async () => {
-    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/meals'] });
+  it('should verify if drink it is in recipe in progress', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/drinks'] });
     waitForElementToBeRemoved(() => screen.getByRole('status'));
-    const corbaEl = await screen.findByRole('heading', { name: /corba/i });
-    userEvent.click(corbaEl);
+    const ggDrink = await screen.findByRole('heading', { name: /GG/i });
+    userEvent.click(ggDrink);
     waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const continueRecipeBtn = screen.queryByRole('button', { name: /continue/i });
+    expect(continueRecipeBtn).not.toBeInTheDocument();
+    localStorage.setItem('inProgressRecipes', JSON.stringify(recipesInProgressStorage));
+    const continueRecipeBtn2 = await screen.findByRole('button', { name: /continue/i });
+    expect(continueRecipeBtn2).toBeInTheDocument();
+  });
 
-    const shareBtn = await screen.findByTestId('share-btn');
-    expect(shareBtn).toBeInTheDocument();
-    const linkCopiedEl = screen.queryByText(/link copied/i);
-    expect(linkCopiedEl).not.toBeInTheDocument();
+  it('should verify if drink recipe is favorite', async () => {
+    renderWithRouter(<Provider><App /></Provider>, { initialEntries: ['/drinks'] });
+    await waitForElementToBeRemoved(() => screen.getByRole('status'));
+    const ggDrink = screen.getByRole('heading', { name: /GG/i });
+    userEvent.click(ggDrink);
+    await waitForElementToBeRemoved(() => screen.getByRole('status'));
+
+    const favoriteBtn = screen.getByTestId('favorite-btn');
+    expect(favoriteBtn).toHaveAttribute('src', whiteHeartIcon);
+    userEvent.click(favoriteBtn);
+    expect(favoriteBtn).toHaveAttribute('src', 'blackHeartIcon.svg');
+    userEvent.click(favoriteBtn);
+    expect(favoriteBtn).toHaveAttribute('src', whiteHeartIcon);
+    localStorage.setItem('favoriteRecipes', JSON.stringify(favoriteRecipesStorage));
+    userEvent.click(favoriteBtn);
   });
 });
---
+++ src/tests/mocks/localStorageMocks.js
@@ -0,0 +1,55 @@
+export const recipesInProgressStorage = {
+  drinks: {
+    15997: [1, 2, 3],
+  },
+  meals: {
+    52977: [1, 2, 3],
+    52771: [1, 2, 3],
+  },
+};
+
+export const doneRecipesStorage = [{
+  id: '15997',
+  type: 'drink',
+  nationality: '',
+  category: 'Ordinary Drink',
+  alcoholicOrNot: 'Optional alcohol',
+  name: 'GG',
+  image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+  doneDate: '2020',
+  tags: ['teste', 'teste'],
+},
+{
+  id: '52771',
+  type: 'meal',
+  nationality: 'Italian',
+  category: 'Vegetarian',
+  alcoholicOrNot: '',
+  name: 'Spicy Arrabiata Penne',
+  image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  doneDate: '2020',
+  tags: ['teste', 'teste'],
+},
+];
+
+export const favoriteRecipesStorage = [
+  // {
+  // id: '15997',
+  // type: 'drink',
+  // nationality: '',
+  // category: 'Ordinary Drink',
+  // alcoholicOrNot: 'Optional alcohol',
+  // name: 'GG',
+  // image: 'https://www.thecocktaildb.com/images/media/drink/vyxwut1468875960.jpg',
+// },
+  {
+    id: '52771',
+    type: 'meal',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+  },
+
+];
